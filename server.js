f85a634c4ac35c31a71c539418840c109de2fe8e
Steve Witman <stevewitman@gmail.com>
2015-03-25 11:38:28 -0600

    added rant model, and read/write

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 // Get packages
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Rant = require('./models/rant');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/rantly');
 
 // Create Express app
 var app = express();
+
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Use environment defined port or 3000
 var port = process.env.PORT || 3000;
@@ -16,9 +27,43 @@ router.get('/', function(req, res) {
   res.json({ message: 'TEST MESSAGE' });
 });
 
-// Register all our routes with /api
+// Create a new route
+var rantsRoute = router.route('/rants');
+
+// Create endpoint /api/rants for POSTS
+rantsRoute.post(function(req, res) {
+  var rant = new Rant();
+  rant.title = req.body.title;
+  rant.body = req.body.body;
+  rant.save(function(err) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Rant Successfully Added', data: rant });
+  });
+});
+
+// Create endpoint /api/rants for GET
+rantsRoute.get(function(req, res) {
+  Rant.find(function(err, rants) {
+    if (err)
+      res.send(err);
+    res.json(rants);
+  });
+});
+
+// Create a route with the /rants/:rant_id prefix
+var rantRoute = router.route('/rants/:rant_id');
+
+// Create endpoint /api/rants/:rant_id for GET
+rantRoute.get(function(req, res) {
+  Rant.findById(req.params.rant_id, function(err, rant) {
+    if (err)
+      res.send(err);
+    res.json(rant);
+  });
+});
+
 app.use('/api', router);
 
-// Start server
 app.listen(port);
-console.log('Insert beer on port ' + port);
+console.log('Insert rant on port ' + port);
